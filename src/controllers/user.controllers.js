21b475f0bb3d4e09fdabf4268e7dff79a83b8815
dilpreet2012 <dilkill2000@gmail.com>
2025-01-30 23:38:41 +0530

    adding a route to get the details of every user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -69,3 +69,14 @@ export const forgetUser=async(req,res)=>{
         res.status(500).json({message:err.message})
     }
 }
+export const getUsers=async(req,res)=>{
+    try
+    {
+        const users=await User.find().select("-password");
+        res.status(200).json(users);
+    }
+    catch(err)
+    {
+        res.status(500).json({message:err.message});
+    }
+}
